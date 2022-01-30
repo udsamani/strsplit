a59e3c77198b34e495b0ce1b728f0fab6d29cac0
Udit Samani <udit.samani@thoughtspot.com>
2022-01-29 20:02:30 -0800

    [C-1] A Basic strsplit with liftetimmes

---
+++ src/lib.rs
@@ -0,0 +1,42 @@
+pub struct StrSplit<'a> {
+    remainder: &'a str,
+    delimiter: &'a str,
+}
+
+
+impl<'a> StrSplit<'a> {
+    pub fn new(haystack: &'a str, delimiter: &'a str) -> Self {
+        Self {
+            remainder: haystack,
+            delimiter
+        }
+    }
+}
+
+
+impl<'a> Iterator for StrSplit<'a> {
+    type Item = &'a str;
+    fn next (&mut self) -> Option<Self::Item> {
+        if let Some(next_delimiter) = self.remainder.find(self.delimiter) {
+            let until_delimiter = &self.remainder[..next_delimiter];
+            self.remainder = &self.remainder[(next_delimiter+self.delimiter.len())..];
+            Some(until_delimiter)
+        } else if self.remainder.is_empty() {
+            // TODO : Bug
+            None
+        } else {
+            let rest = self.remainder;
+            self.remainder = "";
+            Some(rest)
+        }
+    }
+}
+
+
+
+#[test]
+fn it_works() {
+    let haystack = "a b c d e";
+    let letters = StrSplit::new(haystack, " ");
+    assert!(letters.eq(vec!["a", "b", "c", "d", "e"].into_iter()));
+}
